74bf825d56f9ed778ad6728b860f1e4359b98f06
sandeep-19 <sandeep.rqs@gmail.com>
2022-10-22 18:39:08 +0530

    first commit

---
+++ public/client/landing.js
@@ -0,0 +1,8 @@
+// document.getElementById("h").innerHTML += "jyufuh
+window.onload = () => {
+  setTimeout(() => {
+    document
+      .getElementById("hinge")
+      .classList.add("animate__animated", "animate__hinge");
+  }, 5000);
+};
