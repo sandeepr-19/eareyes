5028ee82dbc0cbe7fe9578924fd36317440a3e93
sandeep-19 <sandeep.rqs@gmail.com>
2022-10-25 21:34:17 +0530

    gsap ani

--- public/client/landing.js
+++ public/client/landing.js
@@ -1,12 +1,3 @@
-// document.getElementById("h").innerHTML += "jyufuh
-// window.onload = () => {
-//   setTimeout(() => {
-//     document
-//       .getElementById("hinge")
-//       .classList.add("animate__animated", "animate__hinge");
-//   }, 5000);
-// };
-
 //nav bar
 const hamburger = document.querySelector(".hamburger");
 const navmenu = document.querySelector(".nav-menu");
@@ -28,11 +19,6 @@ gsap.registerPlugin(ScrollTrigger);
 gsap
   .timeline()
   .from("#title", {
-    // scrollTrigger: {
-    //   trigger: "#title",
-    //   pin: true,
-    //   scrub: true,
-    // },
     scale: 0,
     opacity: 0,
     duration: 2,
@@ -40,7 +26,92 @@ gsap
   .from("#hero-dep", { scale: 0, opacity: 0, duration: 2 }, "-=1")
   .to("#boom", { duration: 3, x: "60vw", ease: "back(2)" })
   .to("#boom", { y: "-10vh", duration: 2 })
-  .to("#boom", { x: "30vw", duration: 3 }, "-=1")
-  .to("#hinge", { duration: 2, rotation: 60 }, "-=2")
-  .to("#hinge", { y: "60vh", duration: 1 }, "-=1")
-  .to("#hinge", { display: "none" });
+  .to("#boom", { x: "30vw", duration: 3 }, "-=2")
+  .to("#hinge", { duration: 1, rotation: 50 }, "-=4")
+  .to("#hinge", { y: "200vh", duration: 1 }, "-=3")
+  .to("#hinge", { display: "none" }, "-=2");
+
+// gsap.from(".AA", {
+//   duration: 8,
+//   x: "-700px",
+//   scrollTrigger: {
+//     trigger: ".AA",
+//     scroller: "#scrollsnap",
+//     toggleActions: "play none none reset",
+//   },
+// });
+
+//1 about
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#ah1",
+      scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top bottom",
+      end: "top 25%",
+    },
+  })
+  .from("#ah1", {
+    duration: 3,
+    x: "-300px",
+  })
+  .from(
+    "#ap1",
+    {
+      duration: 3,
+      scale: 0,
+      opacity: 0,
+    },
+    "-=1"
+  );
+
+//2 about
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#ah2",
+      scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top bottom",
+      end: "top 25%",
+    },
+  })
+  .from("#ah2", {
+    duration: 3,
+    x: "-300px",
+  })
+  .from(
+    "#ap2",
+    {
+      duration: 3,
+      scale: 0,
+      opacity: 0,
+    },
+    "-=1"
+  );
+
+//3 about
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#ah3",
+      scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top bottom",
+      end: "top 25%",
+    },
+  })
+  .from("#ah3", {
+    duration: 3,
+    x: "-300px",
+  })
+  .from(
+    "#ap3",
+    {
+      duration: 3,
+      scale: 0,
+      opacity: 0,
+    },
+    "-=1"
+  );
