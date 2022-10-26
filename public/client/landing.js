925943427a00aa8012ef6d220549dd8ba31f1f30
sandeep-19 <sandeep.rqs@gmail.com>
2022-10-26 13:09:06 +0530

    events page

--- public/client/landing.js
+++ public/client/landing.js
@@ -46,7 +46,7 @@ gsap
   .timeline({
     scrollTrigger: {
       trigger: "#ah1",
-      scroller: "#scrollsnap",
+      // scroller: "#scrollsnap",
       scrub: 1,
       start: "top bottom",
       end: "top 25%",
@@ -54,7 +54,7 @@ gsap
   })
   .from("#ah1", {
     duration: 3,
-    x: "-300px",
+    x: "-50vw",
   })
   .from(
     "#ap1",
@@ -71,7 +71,7 @@ gsap
   .timeline({
     scrollTrigger: {
       trigger: "#ah2",
-      scroller: "#scrollsnap",
+      // scroller: "#scrollsnap",
       scrub: 1,
       start: "top bottom",
       end: "top 25%",
@@ -79,7 +79,7 @@ gsap
   })
   .from("#ah2", {
     duration: 3,
-    x: "-300px",
+    x: "-50vw",
   })
   .from(
     "#ap2",
@@ -96,7 +96,7 @@ gsap
   .timeline({
     scrollTrigger: {
       trigger: "#ah3",
-      scroller: "#scrollsnap",
+      //scroller: "#scrollsnap",
       scrub: 1,
       start: "top bottom",
       end: "top 25%",
@@ -104,7 +104,7 @@ gsap
   })
   .from("#ah3", {
     duration: 3,
-    x: "-300px",
+    x: "-50vw",
   })
   .from(
     "#ap3",
@@ -115,3 +115,133 @@ gsap
     },
     "-=1"
   );
+
+//event pin
+ScrollTrigger.create({
+  trigger: "#ee",
+  start: "top 10%",
+  //scroller: "#scrollsnap",
+  endTrigger: "#schedule",
+  end: "top 80%",
+  pin: "#ee",
+});
+
+//event1
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#ee",
+      //scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top center",
+      end: "top 25%",
+    },
+  })
+  .from("#eh1", {
+    duration: 3,
+    x: "-60vw",
+  })
+  .from("#ehh1", {
+    duration: 3,
+    x: "60vw",
+  });
+
+//event2
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#eh2",
+      //scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top 70%",
+      end: "top 25%",
+    },
+  })
+  .from("#eh2", {
+    duration: 3,
+    x: "-60vw",
+  })
+  .from("#ehh2", {
+    duration: 3,
+    x: "60vw",
+  });
+
+//event3
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#eh3",
+      //scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top 70%",
+      end: "top 25%",
+    },
+  })
+  .from("#eh3", {
+    duration: 3,
+    x: "-60vw",
+  })
+  .from("#ehh3", {
+    duration: 3,
+    x: "60vw",
+  });
+
+//event4
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#eh4",
+      //scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top 70%",
+      end: "top 25%",
+    },
+  })
+  .from("#eh4", {
+    duration: 3,
+    x: "-60vw",
+  })
+  .from("#ehh4", {
+    duration: 3,
+    x: "60vw",
+  });
+
+//event5
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#eh5",
+      //scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top 70%",
+      end: "top 25%",
+    },
+  })
+  .from("#eh5", {
+    duration: 3,
+    x: "-60vw",
+  })
+  .from("#ehh5", {
+    duration: 3,
+    x: "60vw",
+  });
+
+//event6
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#eh6",
+      //scroller: "#scrollsnap",
+      scrub: 1,
+      start: "top 70%",
+      end: "top 25%",
+    },
+  })
+  .from("#eh6", {
+    duration: 3,
+    x: "-60vw",
+  })
+  .from("#ehh6", {
+    duration: 3,
+    x: "60vw",
+  });
